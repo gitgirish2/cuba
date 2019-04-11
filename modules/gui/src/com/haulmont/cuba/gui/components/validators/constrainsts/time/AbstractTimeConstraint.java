9680226690b1d354c3e7ef148f0862aa118f0e85
Roman Pinyazhin <pinyazhin@haulmont.com>
2019-04-11 17:59:33 +0400

    Improve declarative validators for Field components

--- modules/gui/src/com/haulmont/cuba/gui/components/validators/constrainsts/time/AbstractTimeConstraint.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/validators/constrainsts/time/AbstractTimeConstraint.java
@@ -9,6 +9,7 @@ import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.TimeSource;
 
 import java.time.*;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -53,6 +54,19 @@ public abstract class AbstractTimeConstraint<T> implements TimeConstraint {
             Date currentValue = timeSource.currentTimestamp();
             return value.compareTo(currentValue);
         }
+
+        @Override
+        public boolean isFutureOrPresent() {
+            Date currentValue = timeSource.currentTimestamp();
+            // set 0 sec and millis to currentValue because we don't know what time format is used
+            // and in case when we set current HH:mm validator will consider that it is not valid
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(currentValue);
+            calendar.clear(Calendar.SECOND);
+            calendar.clear(Calendar.MILLISECOND);
+
+            return value.compareTo(calendar.getTime()) >= 0;
+        }
     }
 
     public static class LocalDateConstraint extends AbstractTimeConstraint<LocalDate> {
@@ -79,6 +93,14 @@ public abstract class AbstractTimeConstraint<T> implements TimeConstraint {
             LocalDateTime currentValue = timeSource.now().toLocalDateTime();
             return value.compareTo(currentValue);
         }
+
+        @Override
+        public boolean isFutureOrPresent() {
+            LocalDateTime currentValue = timeSource.now().toLocalDateTime();
+            // set 0 sec and nano to currentValue because we don't know what time format is used
+            // and in case when we set current HH:mm validator will consider that it is not valid
+            return value.compareTo(currentValue.withSecond(0).withNano(0)) >= 0;
+        }
     }
 
     public static class LocalTimeConstraint extends AbstractTimeConstraint<LocalTime> {
@@ -91,6 +113,14 @@ public abstract class AbstractTimeConstraint<T> implements TimeConstraint {
         public int compareValueWithCurrent() {
             LocalTime currentValue = timeSource.now().toLocalTime();
             return value.compareTo(currentValue);
+        }
+
+        @Override
+        public boolean isFutureOrPresent() {
+            LocalTime currentValue = timeSource.now().toLocalTime();
+            // set 0 sec and nano to currentValue because we don't know what time format is used
+            // and in case when we set current HH:mm validator will consider that it is not valid
+            return value.compareTo(currentValue.withSecond(0).withNano(0)) >= 0;
         }
     }
 
@@ -105,6 +135,14 @@ public abstract class AbstractTimeConstraint<T> implements TimeConstraint {
             OffsetTime currentValue = timeSource.now().toOffsetDateTime().toOffsetTime();
             return value.compareTo(currentValue);
         }
+
+        @Override
+        public boolean isFutureOrPresent() {
+            OffsetTime currentValue = timeSource.now().toOffsetDateTime().toOffsetTime();
+            // set 0 sec and nano to currentValue because we don't know what time format is used
+            // and in case when we set current HH:mm validator will consider that it is not valid
+            return value.compareTo(currentValue.withSecond(0).withNano(0)) >= 0;
+        }
     }
 
     public static class OffsetDateTimeConstraint extends AbstractTimeConstraint<OffsetDateTime> {
@@ -117,6 +155,14 @@ public abstract class AbstractTimeConstraint<T> implements TimeConstraint {
         public int compareValueWithCurrent() {
             OffsetDateTime currentValue = timeSource.now().toOffsetDateTime();
             return value.compareTo(currentValue);
+        }
+
+        @Override
+        public boolean isFutureOrPresent() {
+            OffsetDateTime currentValue = timeSource.now().toOffsetDateTime();
+            // set 0 sec and nano to currentValue because we don't know what time format is used
+            // and in case when we set current HH:mm validator will consider that it is not valid
+            return value.compareTo(currentValue.withSecond(0).withNano(0)) >= 0;
         }
     }
 }
